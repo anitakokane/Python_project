0d95f67aff636f0a87aec57b8db6052b7eb4ef87
Anita Kokane <137875874+anitakokane@users.noreply.github.com>
2025-09-04 16:55:47 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,28 @@
+print("Welcome to interactive personal data collecter")
+
+print("This application takes your personal data and display it in structured format")
+print()
+name=input("Enter your name=")
+age=int(input("Enter your age="))
+height=float(input("Enter your height in meters="))
+num=int(input("Enter your favourite Number="))
+print()
+print("Hello",name,"!","Welcome to interactiove personal data collecter,we got you data like your age is ",age,",your height is ",height,"and your favourite number is ",num)
+
+print()
+print("Name:",name,type(name),id(name))
+print("Age:",age,type(age),id(age))
+print("Height:",height,type(height),id(height))
+print("Favourite Number:",num,type(num),id(num))
+
+print()
+print("From your age we have calculated your Birth year :")
+print("Your birth year is :",2025-age,"(from your given age ,which is:",age,")")
+
+print()
+print("Thank you so much for using interactive personal data collecter , we hope you have a amazing experience , Have a good day")
+
+
+
+
+
